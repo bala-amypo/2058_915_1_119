9537859ceed07ac693e43d7ff9e10b30de151121
bala-amypo <bala@amypo.in>
2025-12-22 04:34:00 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++
@@ -1,4 +0,0 @@
-“Indha project-la stock portfolio-va manage pannuvom,
- thresholds set panni stock exposure calculate panni 
- risk high-aa illa low-aa nu analyze pannuvom, 
- idhu full-aa Spring Boot layered architecture-la irukum.”
