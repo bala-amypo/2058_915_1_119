9aafcd8da49335abd6a9fe7059ab63ecdeb77a23
bala-amypo <bala@amypo.in>
2025-12-26 04:58:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskAnalysisServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskAnalysisServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.RiskAnalysisResult;
+import com.example.demo.repository.*;
 import com.example.demo.service.RiskAnalysisService;
+import com.example.demo.model.*;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
